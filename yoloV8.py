21993dbf5109585220d2bc4c0b3bcd795ebbe395
kaipainenoskari <oskari.kaipainen@hotmail.com>
2023-06-11 21:10:28 +0300

    Changed YOLO to use GPU instead of CPU

--- yoloV8.py
+++ yoloV8.py
@@ -3,30 +3,37 @@
 import cv2
 from ultralytics import YOLO
 import numpy as np
+import torch
 
-lines = []
+classArray = []
 with open("classes.txt") as file:
-    lines = [line.rstrip() for line in file]
+    classArray = [line.rstrip() for line in file]
 
-#print(lines)
+#print(classArray)
 
 model = YOLO("yolov8m.pt")
 
 cap = cv2.VideoCapture("training_videos/NYC_walking.mp4")
+# Starting from frame 600
+cap.set(cv2.CAP_PROP_POS_FRAMES, 10000)
+
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
+    
+    frame = cv2.resize(frame, (1440, 810))
 
-    results = model(frame)
+    # Use GPU instead of CPU -> improve performance ~x10
+    results = model(frame, device='1')
     bboxes = np.array(results[0].boxes.xyxy.cpu(), dtype="int")
     classes = np.array(results[0].boxes.cls.cpu(), dtype="int")
 
     for cls, bbox in zip(classes, bboxes):
         (x, y, x2, y2) = bbox
 
-        cv2.putText(frame, lines[cls], (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
+        cv2.putText(frame, classArray[cls], (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
         cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 255), 2)
 
     cv2.imshow("Image", frame)
